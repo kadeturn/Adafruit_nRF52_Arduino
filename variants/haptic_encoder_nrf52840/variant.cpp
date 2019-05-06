edb978ba0ccd920d9db8aa1e5a162b213621e4c2
Kade_3Dx <kturner@3dconnexion.com>
2019-05-06 09:44:42 +0200

    added haptic files

---
+++ variants/haptic_encoder_nrf52840/variant.cpp
@@ -0,0 +1,62 @@
+
+
+#include "variant.h"
+#include "wiring_constants.h"
+#include "wiring_digital.h"
+#include "nrf.h"
+
+#include "usb.h"
+
+
+const uint32_t g_ADigitalPinMap[] =
+{
+  // Switch Out
+  2,	// D0	R_LED_OUT	AIN0
+  3,	// D1	G_LED_OUT	AIN1
+  20,	// D2	SW2_OUT
+  23,	// D3	SW1_OUT
+  22,	// D4	TOP_OUT
+
+  // Switch IN
+  30,	// D5	R_LED_IN	AIN2
+  28,	// D6	G_LED_IN	AIN3
+  21,	// D7	SW2_IN
+  19,	// D8	SW1_IN
+  16,	// D9	TOP_IN
+ 
+  // Encoder 
+  17,	// D10	CS
+  15,	// D11	MISO
+  13,	// D12	MOSI
+  14,	// D13	SCK
+ 
+  // DRV2605 
+  40,	// D14	TRIG_LRA P1.08
+  11,	// D15	SCL
+  12,	// D16	SDA
+  
+  // Misc
+  42,	// D17	LED1
+  43,	// D18	LED2
+  34,	// D19	SWITCH
+
+  // Uart
+  25,	// D20	TX
+  24	// D21	RX
+   
+};
+
+
+void initVariant()
+{
+  // LED1 & LED2
+  pinMode(PIN_LED1, OUTPUT);
+  ledOff(PIN_LED1);
+
+  pinMode(PIN_LED2, OUTPUT);
+  ledOff(PIN_LED2);
+
+  // USB init  TODO may have trouble with ISR when SD is enabled
+  usb_init();
+}
+
---
+++ variants/haptic_encoder_nrf52840/variant.h
@@ -0,0 +1,120 @@
+#ifndef _VARIANT_HAPTIC_ENCODER_NRF52840_
+#define _VARIANT_HAPTIC_ENCODER_NRF52840_
+
+/** Master clock frequency */
+#define VARIANT_MCK       (64000000ul)
+
+#define USE_LFXO      // Board uses 32khz crystal for LF
+// define USE_LFRC    // Board uses RC for LF
+
+
+/*----------------------------------------------------------------------------
+ *        Headers
+ *----------------------------------------------------------------------------*/
+
+#include "WVariant.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+
+// Number of pins defined in PinDescription array
+#define PINS_COUNT           (21)
+#define NUM_DIGITAL_PINS     (21)
+#define NUM_ANALOG_INPUTS    (4)
+#define NUM_ANALOG_OUTPUTS   (0)
+
+// LEDs
+#define PIN_LED1             (17)
+#define PIN_LED2             (18)
+
+#define LED_BUILTIN          PIN_LED2
+#define LED_CONN             PIN_LED1
+
+#define LED_GREEN            PIN_LED2
+#define LED_BLUE             PIN_LED1
+
+#define LED_STATE_ON         1         // State when LED is litted
+
+// Buttons
+#define PIN_SWITCH             (19)
+
+/*
+ * Analog pins
+ */
+#define PIN_A0               (0)
+#define PIN_A1               (1)
+#define PIN_A2               (5)
+#define PIN_A3               (6)
+
+static const uint8_t A0  = PIN_A0 ;
+static const uint8_t A1  = PIN_A1 ;
+static const uint8_t A2  = PIN_A2 ;
+static const uint8_t A3  = PIN_A3 ;
+#define ADC_RESOLUTION    14
+
+// Other pins
+#define PIN_DFU            (19)
+
+/*
+ * Serial interfaces
+ */
+// Serial
+#define PIN_SERIAL_RX       (21) // P0.24
+#define PIN_SERIAL_TX       (20) // P0.25
+
+/*
+ * SPI Interfaces
+ */
+#define SPI_INTERFACES_COUNT 1
+
+#define PIN_SPI_MISO         (11) // P0.15
+#define PIN_SPI_MOSI         (12) // P0.13
+#define PIN_SPI_SCK          (13) // P0.14
+
+static const uint8_t SS   = (10) ;  // P0.17
+static const uint8_t MOSI = PIN_SPI_MOSI ;
+static const uint8_t MISO = PIN_SPI_MISO ;
+static const uint8_t SCK  = PIN_SPI_SCK ;
+
+/*
+ * Wire Interfaces
+ */
+#define WIRE_INTERFACES_COUNT 1
+
+#define PIN_WIRE_SDA         (16) // P0.12
+#define PIN_WIRE_SCL         (15) // P0.11
+
+static const uint8_t SDA = PIN_WIRE_SDA;
+static const uint8_t SCL = PIN_WIRE_SCL;
+
+/*
+ * QSPI interface for external flash
+ */
+#define PIN_QSPI_SCK         8
+#define PIN_QSPI_CS          2
+#define PIN_QSPI_DATA0       10
+#define PIN_QSPI_DATA1       4
+#define PIN_QSPI_DATA2       3
+#define PIN_QSPI_DATA3       7
+
+/*
+  19,  // D8 is QSPI CLK on P0.19
+  20,  // D2 is QSPI CS on P0.20
+  17,  // D10 is QSPI Data 0 on P0.17
+  22,  // D4 is QSPI Data 1 on P0.22
+  23,  // D3 is QSPI Data 2 on P0.23
+  21,  // D7 is QSPI Data 3 on P0.21
+*/
+
+
+// On-board QSPI Flash
+// If EXTERNAL_FLASH_DEVICES is not defined, all supported devices will be used
+#define EXTERNAL_FLASH_DEVICES   GD25Q16C
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
